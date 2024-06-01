078b88bd63dfdaf19f326c326663b746435f9e0d
ismoil <ismoiljoraxonov1@gmail.com>
2024-06-01 23:44:49 +0900

    sinflar yozildi

---
+++ src/main/java/com/example/calendar1/Calendar1Application.java
@@ -0,0 +1,13 @@
+package com.example.calendar1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calendar1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calendar1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calendar1/Calendar1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calendar1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calendar1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
